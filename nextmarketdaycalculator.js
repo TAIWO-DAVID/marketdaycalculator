1dd3d92e549905cc2152695f109598578656c6fe
Taiwo David <taiwodavid0027@gmail.com>
2023-11-14 21:23:30 +0100

    Changes made and bug fixed, no more undefined response

--- nextmarketdaycalculator.js
+++ nextmarketdaycalculator.js
@@ -1,20 +1,27 @@
-
 var displayLastMarket = document.getElementById("displayLastMarket");
 var displayNextMarket = document.getElementById("displayNextMarket");
 var nextMarketDay = document.getElementById("nextMarketDay");
 
-// console.log("Selected market interval is ", marketInterval);
-
 // Weekdays index
-weekDaysArray = {
-  1: "Sunday",
-  2: "Monday",
-  3: "Tuesday",
-  4: "Wednesday",
-  5: "Thursday",
-  6: "Friday",
-  7: "Saturday",
-};
+// var weekDaysArray = {
+//   1: "Sunday",
+//   2: "Monday",
+//   3: "Tuesday",
+//   4: "Wednesday",
+//   5: "Thursday",
+//   6: "Friday",
+//   7: "Saturday",
+// };
+
+var weekDaysArray = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
 
 displayNextMarket.addEventListener("click", marketDayCalculation);
 // displayNextMarket.addEventListener("change", marketDayCalculation);
@@ -25,15 +32,11 @@ function marketDayCalculation() {
   var lastMarketDay = document.getElementById("lastMarketDay");
   var marketInterval = document.getElementById("marketInterval").value;
 
-
   var lastMarketDayNumber = lastMarketDay.value;
-  var selectedLastMarketDay = lastMarketDay.options[lastMarketDay.selectedIndex].innerText;
-  let dayIndex = Number(lastMarketDayNumber) + Number(marketInterval) + 1;
+  var selectedLastMarketDay =
+    lastMarketDay.options[lastMarketDay.selectedIndex].innerText;
+  let dayIndex = (parseInt(lastMarketDayNumber) + parseInt(marketInterval)) % 7;
 
-
-
-  dayIndex = dayIndex % 7;
-  weekDaysArray[dayIndex];
   displayLastMarket.innerText =
     "The last market day selected is " + selectedLastMarketDay;
   console.log("The next market day is ", weekDaysArray[dayIndex]);
